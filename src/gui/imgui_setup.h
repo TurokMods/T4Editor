ecae7b7d6e28191e5970aff1770254617e2ed6c9
Michael DeCicco <mdecicco8@gmail.com>
2017-10-04 20:24:11 -0400

    Okay, now we're good

--- src/gui/imgui_setup.h
+++ src/gui/imgui_setup.h
@@ -1,3 +1,4 @@
+#pragma once
 // ImGui GLFW binding with OpenGL3 + shaders
 // In this binding, ImTextureID is used to store an OpenGL 'GLuint' texture identifier. Read the FAQ about ImTextureID in imgui.cpp.
 
