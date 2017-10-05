df7f928ea761c791b8a11ede7f87ab60bd81cbbd
Michael DeCicco <mdecicco8@gmail.com>
2017-10-05 04:13:55 -0400

    Remove glUseProgram(0)

--- src/app.cpp
+++ src/app.cpp
@@ -147,7 +147,6 @@ namespace t4editor {
             glEnableVertexAttribArray(0);
             glBindBuffer(GL_ARRAY_BUFFER, VertexBufferID);
             glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
-            glUseProgram(0);
             glDrawArrays(GL_TRIANGLES, 0, 3);
             glDisableVertexAttribArray(0);
             
